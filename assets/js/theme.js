0e9509663358af565a6c766452200d08a9832b5f
Sokolovskiss <63672924+Sokolovskiss@users.noreply.github.com>
2021-03-09 19:35:55 +0200

    Add files via upload

---
+++ assets/js/theme.js
@@ -0,0 +1,16 @@
+function showMenu(){   
+    var x = document.getElementById('OM');
+    if(x.style.display === "block"){
+        x.style.display = "none";
+    }else{ x.style.display = "block"}
+}
+
+window.onscroll = function() {scrollUp()};
+
+function scrollUp() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    document.getElementById("fixedAnchorLink").style.display = 'block';
+  } else {
+    document.getElementById("fixedAnchorLink").style.display = 'none';
+  }
+}
